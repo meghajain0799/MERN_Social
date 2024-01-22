363e07ea534e6dc2edbb7effc97e0c40d5788252
Megha <54746218+meghajain0799@users.noreply.github.com>
2024-01-22 21:52:42 +0530

    changed port

--- api/index.js
+++ api/index.js
@@ -50,6 +50,8 @@ app.use("/api/users", userRoute)
 app.use("/api/auth", authRoute)
 app.use("/api/posts", postRoute)
 
-app.listen(8800, ()=> {
+const port = process.env.PORT || 8800;
+
+app.listen(port, ()=> {
     console.log("Backend server is running!")
 })
