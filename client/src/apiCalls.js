8d66cb43fda0bbd6b220d095c7061fd682d5241d
Megha <54746218+meghajain0799@users.noreply.github.com>
2024-01-24 16:06:06 +0530

    changed proxy link

--- client/src/apiCalls.js
+++ client/src/apiCalls.js
@@ -1,9 +1,11 @@
 import axios from "axios"
 
 export const loginCall = async (userCredential,dispatch) => {
+    // const BASE_URL = process.env.BASE_URL
+
     dispatch({type: "LOGIN_START"})
     try{
-        const res = await axios.post(`${BASE_URL}auth/login`, userCredential);
+        const res = await axios.post(`auth/login`, userCredential);
         dispatch({type:"LOGIN_SUCCESS", payload:
         res.data });
     } catch (err) {
