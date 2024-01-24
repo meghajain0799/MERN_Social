9dfbffd38e03fef30e155aabea01b85793cef1ae
Megha <54746218+meghajain0799@users.noreply.github.com>
2024-01-24 16:55:15 +0530

    cors updated

--- api/index.js
+++ api/index.js
@@ -21,6 +21,14 @@ mongoose.connect(process.env.MONGO_URL,
     console.log("MongoDB connected")
 }));
 
+const cors = require('cors');
+const corsOptions ={
+    origin:'http://localhost:3000', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
+
 app.use("/images",express.static(path.join(__dirname, "public/images")))
 
 //middleware
